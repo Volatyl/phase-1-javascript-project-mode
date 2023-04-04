238dfd84c9ee2157444b8ffeaa954ab8b0526b64
Kennedy Muinde <kennedymuinde@gmail.com>
2023-04-04 11:31:17 +0300

    Got the cart to add items

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 const products = document.getElementById("products");
+const cartItems = document.getElementById("cartItems");
 
 //Display all products when the page loads
 window.addEventListener("DOMContentLoaded", () => {
   fetch("https://fakestoreapi.com/products")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       data.forEach((product) => {
-        console.log(product);
         const li = document.createElement("li");
-        li.setAttribute("id", "items");
-        li.innerHTML = `
-        <img src="${product.image}"/>
+        li.classList.add("items");
+        li.innerHTML = `<div id="liContainer>
+        <div id="imgContainer">
+          <img src="${product.image}"/>
+        </div>
         <p>${product.title}</p>
-        <p>${product.price}</p>
-        <p id="addToCart">Add To Cart</p>
+        <p id="price">Price: Ksh ${product.price}</p>
+        <p class="addToCart">Add To Cart</p>
+        </div>
         `;
         products.appendChild(li);
+
+        const addToCartBtn = li.querySelectorAll(".addToCart");
+        addToCartBtn.forEach((btn) => {
+          btn.addEventListener("click", (e) => {
+            console.log(e);
+            const cartLi = document.createElement("li");
+            cartLi.innerHTML = `
+            <div class="cartItemsContainer">
+              <img src="${product.image}" id="cartItemImage">
+              <p>${product.title}</p>
+              <p>${product.price}</p>
+            </div>
+            `;
+            cartItems.appendChild(cartLi);
+          });
+        });
       });
     });
-
-  //Function to display products
 });
