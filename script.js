b474409093ced3caa9df296b9fb2d65b2a1109f8
Kennedy Muinde <kennedymuinde@gmail.com>
2023-04-04 12:50:11 +0300

    added function to calculate cart total

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const cartItems = document.getElementById("cartItems");
 
 //Display all products when the page loads
 window.addEventListener("DOMContentLoaded", () => {
-  fetch("https://fakestoreapi.com/products")
+  fetch("http://localhost:3000/products")
     .then((res) => res.json())
     .then((data) => {
       data.forEach((product) => {
         const li = document.createElement("li");
         li.classList.add("items");
-        li.innerHTML = `<div id="liContainer>
+        li.innerHTML = `<div id="liContainer">
         <div id="imgContainer">
           <img src="${product.image}"/>
         </div>
         <p>${product.title}</p>
-        <p id="price">Price: Ksh ${product.price}</p>
+        <p class="price">Price: Ksh ${product.price}</p>
         <p class="addToCart">Add To Cart</p>
         </div>
         `;
@@ -23,17 +23,32 @@ window.addEventListener("DOMContentLoaded", () => {
         //add items to cart
         const addToCartBtn = li.querySelectorAll(".addToCart");
         addToCartBtn.forEach((btn) => {
-          btn.addEventListener("click", (e) => {
-            console.log(e);
+          btn.addEventListener("click", () => {
             const cartLi = document.createElement("li");
             cartLi.innerHTML = `
             <div class="cartItemsContainer">
               <img src="${product.image}" id="cartItemImage">
-              <p>${product.title}</p>
-              <p>${product.price}</p>
+              <div class="itemDetails"
+                <p>${product.title}</p> 
+                <p class="price">Price: ${product.price}</p>
+              </div>
             </div>
             `;
             cartItems.appendChild(cartLi);
+            const cartLiItems = cartLi.getElementsByTagName("li");
+            console.log(getTotal());
+
+            //function to calculate the total of the cart items
+            function getTotal() {
+              let total = 0;
+              for (let i = 0; i < cartLiItems.length; i++) {
+                const item = cartLiItems[i];
+                const priceEl = item.querySelector(".price");
+                const price = parseInt(priceEl.textConten.split(":")[1]);
+                total += price;
+              }
+              return total;
+            }
           });
         });
       });
