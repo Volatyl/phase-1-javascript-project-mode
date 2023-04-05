b0fa760b3f7019d3b2578bf355434c73256c9f5e
Kennedy Muinde <kennedymuinde@gmail.com>
2023-04-05 19:57:12 +0300

    Added a remove functionality

--- script.js
+++ script.js
@@ -2,8 +2,23 @@ const products = document.getElementById("products");
 const cartItems = document.getElementById("cartItems");
 let total = document.getElementById("totalAmout");
 const searchKeyWord = document.getElementById("searchInput");
-
+const cart = document.querySelector(".cart");
 const searchBtn = document.getElementById("searchButton");
+
+//Function to delete item from server
+function deleteItemFromServer(itemId) {
+  fetch(`http://localhost:3000/products/${itemId}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log(`Item ${itemId} deleted from server`);
+      } else {
+        console.error(`Failed to delete item ${itemId} from server`);
+      }
+    })
+    .catch((error) => console.error(error));
+}
 
 //Function to display all products. Encopanses everything
 function displayProducts(data) {
@@ -17,9 +32,18 @@ function displayProducts(data) {
     <p>${product.title}</p>
     <p class="price">Price: Ksh ${product.price}</p>
     <p class="addToCart">Add To Cart</p>
+    <button class="removeItem">Remove</button>
     </div>
     `;
+
     products.appendChild(li);
+
+    //remove items from product list
+    const removeBtn = li.querySelector(".removeItem");
+    removeBtn.addEventListener("click", () => {
+      li.remove();
+      deleteItemFromServer(product.id);
+    });
 
     //add items to cart
     const addToCartBtn = li.querySelectorAll(".addToCart");
@@ -28,13 +52,13 @@ function displayProducts(data) {
         const cartItem = document.createElement("div");
         cartItem.innerHTML = `
         <div class="cartItemsContainer">
-          <img src="${product.image}" id="cartItemImage">
-          <div class="itemDetails">
+          <img src="${product.image}" id="cartItemImage">        
             <p>${product.title}</p> 
-            <p class="price">Price: ${product.price}</p>
-          </div>
+            <p class="price">Price: ${product.price}</p>        
+          <button class="removeCartItem">Remove</button>
         </div>
         `;
+
         cartItems.appendChild(cartItem);
         const cartItemsList =
           cartItems.getElementsByClassName("cartItemsContainer");
@@ -51,6 +75,16 @@ function displayProducts(data) {
           }
           return total;
         }
+
+        // add event listener to removeCartItem button
+        const removeCartItemBtns =
+          cartItems.querySelectorAll(".removeCartItem");
+        removeCartItemBtns.forEach((btn) => {
+          btn.addEventListener("click", () => {
+            btn.parentElement.remove();
+            total.textContent = `Ksh: ${getTotal()}`;
+          });
+        });
       });
     });
   });
@@ -61,13 +95,13 @@ window.addEventListener("DOMContentLoaded", () => {
   filterProducts();
 });
 
-//Function to add serch functionality
+//Function to add search functionality
 function filterProducts(category) {
   fetch("http://localhost:3000/products")
     .then((res) => res.json())
     .then((data) => {
+      // filter products by category
       if (category) {
-        console.log(category);
         data = data.filter((product) => product.category === category);
       }
       products.innerHTML = "";
@@ -77,6 +111,6 @@ function filterProducts(category) {
 
 //Call the search
 searchBtn.addEventListener("click", (e) => {
-  const category = searchKeyWord.value;
+  const category = searchKeyWord.value.toLowerCase();
   filterProducts(category);
 });
